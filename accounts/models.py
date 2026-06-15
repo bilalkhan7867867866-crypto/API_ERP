5fdb2ea3e230a33a18aa5a23a6a2124959959c4e
bilalkhan7867867866-crypto <bilalkhan7867867866@gmail.com>
2026-06-15 15:18:18 +0530

    create app accounts

---
+++ accounts/models.py
@@ -0,0 +1,32 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+
+    class Role(models.TextChoices):
+        ADMIN = "ADMIN", "Admin"
+        HOD = "HOD", "HOD"
+        TEACHER = "TEACHER", "Teacher"
+        STUDENT = "STUDENT", "Student"
+
+    role = models.CharField(
+        max_length=20,
+        choices=Role.choices
+    )
+
+    phone_number = models.CharField(
+        max_length=15,
+        blank=True
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
+
+    def __str__(self):
+        return self.username
