975b5c7feb8efa9f485480eca0c299cfee271327
bilalkhan7867867866-crypto <bilalkhan7867867866@gmail.com>
2026-06-15 15:39:04 +0530

    add files

--- college_erp/settings.py
+++ college_erp/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'accounts',
+    'departments',
+    'profiles',
 ]
 
 MIDDLEWARE = [
---
+++ departments/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class Department(models.Model):
+
+    name = models.CharField(
+        max_length=100,
+        unique=True
+    )
+
+    code = models.CharField(
+        max_length=20,
+        unique=True
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def __str__(self):
+        return self.name
